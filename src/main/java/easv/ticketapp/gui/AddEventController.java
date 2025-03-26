8fa17121652f76a2783cd9cde8c6d7e78025e873
Stefan Rubelov <71753778+stefanrubelov@users.noreply.github.com>
2025-03-26 10:57:49 +0100

    Revert "Prep for implementing add/edit events (Need to pull first)"

--- src/main/java/easv/ticketapp/gui/AddEventController.java
+++
@@ -1,7 +0,0 @@
-package easv.ticketapp.gui;
-
-public class AddEventController {
-    public void Add(){
-
-    }
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,7 +17,4 @@ module easv.ticketapp {
 
     exports easv.ticketapp;
     exports easv.ticketapp.gui;
-
-    opens easv.ticketapp.gui to javafx.fxml;
 }
-
