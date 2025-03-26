4a628560f1f8f21a366d5ef5b2c87d6d42aef82d
elRoobz <robendudeofficial@gmail.com>
2025-03-26 09:36:40 +0100

    styling fixes

--- src/main/java/easv/ticketapp/Main.java
+++ src/main/java/easv/ticketapp/Main.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("coordinators-scene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("add-coordinator-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
