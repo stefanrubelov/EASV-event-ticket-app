261c0a55fece3cedade9c488968d96a89ea76dc2
Stefan Rubelov <stefanrubelov@gmail.com>
2025-03-28 09:30:08 +0100

    toggle button to show/hide password field in login

--- src/main/java/easv/ticketapp/Main.java
+++ src/main/java/easv/ticketapp/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
 
